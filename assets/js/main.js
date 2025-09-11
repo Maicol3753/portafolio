7a298b5a23bde8698464548f8a9a844bb4e0cb61
Maicol3753 <maicol.salazar02@inacapmail.cl>
2025-09-11 15:12:29 -0300

    Primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+(function(){
+  // Marcar activo según la URL
+  const path = location.pathname.split('/').pop() || 'index.html';
+  document.querySelectorAll('.nav-links a').forEach(a => {
+    const href = a.getAttribute('href');
+    if (href === path) a.classList.add('active');
+  });
+})();
